b599cc85ef8635a19f48d945323920a72ef747ec
Batmend Ganbaatar <Batmend Ganbaatar>
2024-01-12 18:38:03 +0800

    fix: navbar tooltip

--- lib/widgets/home/navbar/navbar_button.dart
+++ lib/widgets/home/navbar/navbar_button.dart
@@ -28,28 +28,31 @@ class NavbarButton extends StatelessWidget {
         fill: (isActive && index != 0) ? 1 : 0,
         color: context.colorScheme.onSecondary,
       ),
-      child: Expanded(
-        child: Material(
-          type: MaterialType.transparency,
-          color: context.colorScheme.onSecondary,
-          shape: const StadiumBorder(),
-          child: InkWell(
-            customBorder: const StadiumBorder(),
-            onTap: () => onTap(index),
-            // splashColor: Colors.red,
-            // splashColor: Theme.of(context).splashColor,
-            // focusColor: Colors.blue,
-            focusColor: Theme.of(context).focusColor,
-            hoverColor: Theme.of(context).hoverColor,
-            // highlightColor: Colors.blue,
-            // highlightColor: Theme.of(context).highlightColor,
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: AnimatedOpacity(
-                opacity: isActive ? 1 : 0.5,
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeOut,
-                child: Icon(icon),
+      child: Tooltip(
+        message: tooltip,
+        child: Expanded(
+          child: Material(
+            type: MaterialType.transparency,
+            color: context.colorScheme.onSecondary,
+            shape: const StadiumBorder(),
+            child: InkWell(
+              customBorder: const StadiumBorder(),
+              onTap: () => onTap(index),
+              // splashColor: Colors.red,
+              // splashColor: Theme.of(context).splashColor,
+              // focusColor: Colors.blue,
+              focusColor: Theme.of(context).focusColor,
+              hoverColor: Theme.of(context).hoverColor,
+              // highlightColor: Colors.blue,
+              // highlightColor: Theme.of(context).highlightColor,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: AnimatedOpacity(
+                  opacity: isActive ? 1 : 0.5,
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeOut,
+                  child: Icon(icon),
+                ),
               ),
             ),
           ),
