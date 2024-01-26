58e238c656c356fff373ce8b8722a21b3556bcaa
Batmend Ganbaatar <batmend@gege.mn>
2024-01-26 15:55:27 +0800

    refactor

--- lib/routes/new_transaction/select_account_sheet.dart
+++ lib/routes/new_transaction/select_account_sheet.dart
@@ -20,23 +20,26 @@ class SelectAccountSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return BottomSheetFrame(
       scrollable: true,
-      child: Column(mainAxisSize: MainAxisSize.min, children: [
-        const SizedBox(height: 16.0),
-        Text(
-          "transaction.edit.selectAccount".t(context),
-          style: context.textTheme.headlineSmall,
-        ),
-        const SizedBox(height: 16.0),
-        ...accounts.map(
-          (account) => ListTile(
-            title: Text(account.name),
-            leading: Icon(account.icon),
-            trailing: const Icon(Symbols.chevron_right_rounded),
-            onTap: () => context.pop(account),
-            selected: currentlySelectedAccountId == account.id,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(height: 16.0),
+          Text(
+            "transaction.edit.selectAccount".t(context),
+            style: context.textTheme.headlineSmall,
           ),
-        ),
-      ]),
+          const SizedBox(height: 16.0),
+          ...accounts.map(
+            (account) => ListTile(
+              title: Text(account.name),
+              leading: Icon(account.icon),
+              trailing: const Icon(Symbols.chevron_right_rounded),
+              onTap: () => context.pop(account),
+              selected: currentlySelectedAccountId == account.id,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
