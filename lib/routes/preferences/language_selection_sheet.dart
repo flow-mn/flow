a50131bcfe341c3017c561a472cf08e76c935c76
Batmend Ganbaatar <batmend@gege.mn>
2024-01-26 15:53:30 +0800

    chore: language selection sheet for pref

--- lib/routes/preferences/language_selection_sheet.dart
+++ lib/routes/preferences/language_selection_sheet.dart
@@ -1,7 +1,13 @@
+import 'package:flow/l10n/flow_localizations.dart';
+import 'package:flow/theme/theme.dart';
+import 'package:flow/widgets/general/bottom_sheet_frame.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class LanguageSelectionSheet extends StatefulWidget {
-  const LanguageSelectionSheet({super.key});
+  final Locale? currentLocale;
+
+  const LanguageSelectionSheet({super.key, this.currentLocale});
 
   @override
   State<LanguageSelectionSheet> createState() => _LanguageSelectionSheetState();
@@ -10,6 +16,26 @@ class LanguageSelectionSheet extends StatefulWidget {
 class _LanguageSelectionSheetState extends State<LanguageSelectionSheet> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BottomSheetFrame(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 16.0),
+            Text(
+              "preferences.language.choose".t(context),
+              style: context.textTheme.headlineSmall,
+            ),
+            const SizedBox(height: 16.0),
+            ...FlowLocalizations.supportedLanguages.map(
+              (locale) => ListTile(
+                title: Text(locale.name),
+                onTap: () => context.pop(locale),
+                selected: widget.currentLocale == locale,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
