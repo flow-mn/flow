cd07c7cbca52e1aefea274b32381ef1042abc9e8
Batmend Ganbaatar <batmend@gege.mn>
2024-01-30 18:53:15 +0800

    fix typo

--- lib/sync/import/mode.dart
+++ lib/sync/import/mode.dart
@@ -4,6 +4,6 @@ enum ImportMode {
 
   /// Merges items with matching `uuid` or `name`, adds everything else
   ///
-  /// If `uuid` matches, it uses `name` from the newest object (`createDate`)
+  /// If `uuid` matches, it uses `name` from the newest object (`createdDate`)
   merge,
 }
