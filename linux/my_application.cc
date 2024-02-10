bd0b97379c3f528ad36710618d0793b4d1fc96cf
Batmend Ganbaatar <batmend@gege.mn>
2024-02-10 17:00:17 +0800

    chore: update launcher name

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -45,13 +45,13 @@ static void my_application_activate(GApplication *application)
   {
     GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "flow");
+    gtk_header_bar_set_title(header_bar, "Flow");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   }
   else
   {
-    gtk_window_set_title(window, "flow");
+    gtk_window_set_title(window, "Flow");
   }
 
   gtk_window_set_default_size(window, 414, 896);
