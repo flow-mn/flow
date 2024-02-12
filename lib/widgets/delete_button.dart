19ee659813c47a38e4c0b355d4973f94fcba86a6
Batmend Ganbaatar <batmend@gege.mn>
2024-02-12 15:47:15 +0800

    chore: delete button style revamp

--- lib/widgets/delete_button.dart
+++ lib/widgets/delete_button.dart
@@ -1,28 +1,27 @@
 import 'package:flow/l10n/extensions.dart';
 import 'package:flow/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class DeleteButton extends StatelessWidget {
   final Widget? label;
 
   final VoidCallback? onTap;
 
+  final BorderRadius borderRadius;
+
   const DeleteButton({
     super.key,
     this.label,
     this.onTap,
+    this.borderRadius = const BorderRadius.all(Radius.circular(16.0)),
   });
-
-  static const BorderRadius borderRadius =
-      BorderRadius.all(Radius.circular(16.0));
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: context.flowColors.expense,
-      shape: const RoundedRectangleBorder(
-        borderRadius: borderRadius,
-      ),
+      color: context.colorScheme.onError,
+      shape: RoundedRectangleBorder(borderRadius: borderRadius),
       elevation: 0.0,
       type: MaterialType.button,
       child: InkWell(
@@ -30,18 +29,25 @@ class DeleteButton extends StatelessWidget {
         borderRadius: borderRadius,
         child: Padding(
           padding: const EdgeInsets.symmetric(
-            horizontal: 24.0,
-            vertical: 12.0,
+            horizontal: 12.0,
+            vertical: 8.0,
           ),
           child: DefaultTextStyle(
             style: TextStyle(
-              color: context.colorScheme.background,
+              color: context.colorScheme.error,
               fontWeight: FontWeight.w500,
             ),
-            child: label ??
-                Text(
-                  "general.delete".t(context),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  Symbols.delete_forever_rounded,
+                  color: context.colorScheme.error,
                 ),
+                const SizedBox(width: 8.0),
+                label ?? Text("general.delete".t(context)),
+              ],
+            ),
           ),
         ),
       ),
