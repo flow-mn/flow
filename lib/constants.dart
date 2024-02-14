c2b31347bf1038b2a0e8fea421057aa9324905f5
Batmend Ganbaatar <batmend@gege.mn>
2024-02-15 03:17:59 +0800

    chore: use non-action cards for support

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,4 +8,6 @@ bool get flowDebugMode => kDebugMode || debugBuild;
 final Uri discordInviteLink = Uri.parse("https://discord.gg/Ndh9VDeZa4");
 final Uri maintainerKoFiLink = Uri.parse("https://ko-fi.com/sadespresso");
 final Uri flowGitHubRepoLink = Uri.parse("https://github.com/flow-mn/flow");
+final Uri flowGitHubIssuesLink =
+    Uri.parse("https://github.com/flow-mn/flow/issues");
 final Uri maintainerGitHubLink = Uri.parse("https://github.com/sadespresso");
--- lib/routes/support_page.dart
+++ lib/routes/support_page.dart
@@ -1,13 +1,19 @@
+import 'package:flow/constants.dart';
 import 'package:flow/data/flow_icon.dart';
 import 'package:flow/l10n/extensions.dart';
 import 'package:flow/theme/theme.dart';
 import 'package:flow/utils/utils.dart';
-import 'package:flow/widgets/action_card.dart';
+import 'package:flow/widgets/button.dart';
 import 'package:flow/widgets/general/flow_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
 class SupportPage extends StatelessWidget {
+  static const EdgeInsets cardPadding = EdgeInsets.symmetric(
+    horizontal: 24.0,
+    vertical: 24.0,
+  );
+
   const SupportPage({super.key});
 
   @override
@@ -21,16 +27,14 @@ class SupportPage extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              Text("support.description".t(context)),
+              Text(
+                "support.description".t(context),
+                style: context.textTheme.bodyLarge,
+              ),
               const SizedBox(height: 16.0),
-              ActionCard(
-                onTap: () => openUrl(
-                    Uri.parse("https://github.com/flow-mn/flow/issues")),
-                builder: (context) => Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 24.0,
-                    vertical: 16.0,
-                  ),
+              Card(
+                child: Padding(
+                  padding: cardPadding,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisSize: MainAxisSize.min,
@@ -50,19 +54,24 @@ class SupportPage extends StatelessWidget {
                         "support.requestFeatures.description".t(context),
                         style: context.textTheme.bodySmall,
                       ),
+                      const SizedBox(height: 8.0),
+                      Button(
+                        trailing: const Icon(Symbols.chevron_right_rounded),
+                        child: Expanded(
+                          child: Text(
+                            "support.requestFeatures.action".t(context),
+                          ),
+                        ),
+                        onTap: () => openUrl(flowGitHubIssuesLink),
+                      )
                     ],
                   ),
                 ),
               ),
               const SizedBox(height: 16.0),
-              ActionCard(
-                onTap: () =>
-                    openUrl(Uri.parse("https://github.com/flow-mn/flow")),
-                builder: (context) => Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 24.0,
-                    vertical: 16.0,
-                  ),
+              Card(
+                child: Padding(
+                  padding: cardPadding,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisSize: MainAxisSize.min,
@@ -82,19 +91,24 @@ class SupportPage extends StatelessWidget {
                         "support.contribute.description".t(context),
                         style: context.textTheme.bodySmall,
                       ),
+                      const SizedBox(height: 8.0),
+                      Button(
+                        trailing: const Icon(Symbols.chevron_right_rounded),
+                        child: Expanded(
+                          child: Text(
+                            "visitGitHubRepo".t(context),
+                          ),
+                        ),
+                        onTap: () => openUrl(flowGitHubRepoLink),
+                      )
                     ],
                   ),
                 ),
               ),
               const SizedBox(height: 16.0),
-              ActionCard(
-                onTap: () =>
-                    openUrl(Uri.parse("https://ko-fi.com/sadespresso")),
-                builder: (context) => Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 24.0,
-                    vertical: 16.0,
-                  ),
+              Card(
+                child: Padding(
+                  padding: cardPadding,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisSize: MainAxisSize.min,
@@ -114,6 +128,16 @@ class SupportPage extends StatelessWidget {
                         "support.donateDeveloper.description".t(context),
                         style: context.textTheme.bodySmall,
                       ),
+                      const SizedBox(height: 8.0),
+                      Button(
+                        trailing: const Icon(Symbols.chevron_right_rounded),
+                        child: Expanded(
+                          child: Text(
+                            "support.donateDeveloper.action".t(context),
+                          ),
+                        ),
+                        onTap: () => openUrl(flowGitHubRepoLink),
+                      )
                     ],
                   ),
                 ),
