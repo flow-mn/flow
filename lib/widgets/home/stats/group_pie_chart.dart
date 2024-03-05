e87cc8d369ac06962675462cacbb377f2bacaf2f
Batmend Ganbaatar <batmend@gege.mn>
2024-03-05 18:35:31 +0800

    chore: add colors to tab

--- lib/widgets/home/stats/group_pie_chart.dart
+++ lib/widgets/home/stats/group_pie_chart.dart
@@ -4,6 +4,7 @@ import 'package:flow/data/money_flow.dart';
 import 'package:flow/entity/account.dart';
 import 'package:flow/entity/category.dart';
 import 'package:flow/l10n/flow_localizations.dart';
+import 'package:flow/theme/primary_colors.dart';
 import 'package:flow/theme/theme.dart';
 import 'package:flow/widgets/general/flow_icon.dart';
 import 'package:flow/widgets/home/stats/legend_list_tile.dart';
@@ -52,9 +53,9 @@ class _GroupPieChartState<T> extends State<GroupPieChart<T>> {
                 if (!event.isInterestedForInteractions ||
                     response == null ||
                     response.touchedSection == null) {
-                  setState(() {
-                    selectedKey = null;
-                  });
+                  // setState(() {
+                  //   selectedKey = null;
+                  // });
                   return;
                 }
 
@@ -67,37 +68,76 @@ class _GroupPieChartState<T> extends State<GroupPieChart<T>> {
               }),
               sectionsSpace: 2.0,
               centerSpaceRadius: 48.0,
-              sections: data.entries
-                  .map((e) => sectionData(data[e.key]!,
-                      showBadge: e.key == selectedKey))
+              startDegreeOffset: -90.0,
+              sections: data.entries.indexed
+                  .map(
+                    (e) => sectionData(
+                      data[e.$2.key]!,
+                      selected: e.$2.key == selectedKey,
+                      index: e.$1,
+                    ),
+                  )
                   .toList(),
             ),
           ),
         ),
-        ...data.entries.map((e) => LegendListTile(
-              key: ValueKey(e.key),
-              leading: resolveBadgeWidget(e.value.associatedData),
-              title: Text(resolveName(e.value.associatedData)),
-              trailing: Text(
-                e.value.totalExpense.moneyCompact,
-                style: context.textTheme.bodyLarge,
-              ),
-              selected: e.key == selectedKey,
-              onTap: () => setState(() => selectedKey = e.key),
-            ))
+        ...(data.entries.toList().indexed.toList()
+              ..sort((a, b) =>
+                  a.$2.value.totalExpense.compareTo(b.$2.value.totalExpense)))
+            .map((e) {
+          final color = primaryColors[e.$1 % primaryColors.length];
+          final backgroundColor = accentColors[e.$1 % primaryColors.length];
+
+          return LegendListTile(
+            key: ValueKey(e.$2.key),
+            color: color,
+            leading: resolveBadgeWidget(
+              e.$2.value.associatedData,
+              color: color,
+              backgroundColor: backgroundColor,
+            ),
+            title: Text(resolveName(e.$2.value.associatedData)),
+            trailing: Text(
+              e.$2.value.totalExpense.moneyCompact,
+              style: context.textTheme.bodyLarge,
+            ),
+            selected: e.$2.key == selectedKey,
+            onTap: () => setState(() => selectedKey = e.$2.key),
+          );
+        })
       ],
     );
   }
 
-  PieChartSectionData sectionData(MoneyFlow<T> flow, {bool showBadge = false}) {
+  PieChartSectionData sectionData(
+    MoneyFlow<T> flow, {
+    bool selected = false,
+    int index = 0,
+  }) {
+    final color = primaryColors[index % primaryColors.length];
+    final backgroundColor = accentColors[index % primaryColors.length];
+
     return PieChartSectionData(
-      color: context.colorScheme.secondary,
+      color: color,
       radius: 80.0,
       value: flow.totalExpense.abs(),
       title: resolveName(flow.associatedData),
       showTitle: false,
-      badgeWidget: showBadge ? resolveBadgeWidget(flow.associatedData) : null,
-      badgePositionPercentageOffset: 1.0,
+      badgeWidget: selected
+          ? resolveBadgeWidget(
+              flow.associatedData,
+              color: color,
+              backgroundColor: backgroundColor,
+            )
+          : null,
+      badgePositionPercentageOffset: 1.1,
+      borderSide: selected
+          ? BorderSide(
+              color: context.colorScheme.primary,
+              width: 2.0,
+              strokeAlign: BorderSide.strokeAlignInside,
+            )
+          : BorderSide.none,
     );
   }
 
@@ -107,18 +147,26 @@ class _GroupPieChartState<T> extends State<GroupPieChart<T>> {
         _ => "???"
       };
 
-  Widget? resolveBadgeWidget(Object? entity) => switch (entity) {
+  Widget? resolveBadgeWidget(Object? entity,
+          {Color? color, Color? backgroundColor}) =>
+      switch (entity) {
         Category category => FlowIcon(
             category.icon,
             plated: true,
+            color: color,
+            plateColor: backgroundColor ?? color?.withAlpha(0x40),
           ),
         Account account => FlowIcon(
             account.icon,
             plated: true,
+            color: color,
+            plateColor: backgroundColor ?? color?.withAlpha(0x40),
           ),
         _ => FlowIcon(
             FlowIconData.emoji("?"),
             plated: true,
+            color: color,
+            plateColor: backgroundColor ?? color?.withAlpha(0x40),
           ),
       };
 }
