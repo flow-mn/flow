2138d9f8c71d8f8a3e1f9dfd1484f1613581f37e
Batmend Ganbaatar <batmend@gege.mn>
2024-03-30 23:31:44 +0800

    chore: bump ver to 0.4.0+37

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 
-const appVersion = "0.3.4+36";
+const appVersion = "0.4.0+37";
 const debugBuild = false;
 
 bool get flowDebugMode => kDebugMode || debugBuild;
