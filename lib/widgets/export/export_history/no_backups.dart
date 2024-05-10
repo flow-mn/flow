ec1a7a7a357bdac3c41fca004edf22538ebb7f9a
Batmend Ganbaatar <batmend@gege.mn>
2024-05-11 00:19:13 +0800

    backup history empty state, fix #152

---
+++ lib/widgets/export/export_history/no_backups.dart
@@ -0,0 +1,40 @@
+import 'package:flow/data/flow_icon.dart';
+import 'package:flow/l10n/extensions.dart';
+import 'package:flow/theme/theme.dart';
+import 'package:flow/widgets/general/flow_icon.dart';
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
+
+class NoBackups extends StatelessWidget {
+  const NoBackups({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "sync.export.history.empty".t(context),
+              textAlign: TextAlign.center,
+              style: context.textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 8.0),
+            FlowIcon(
+              FlowIconData.icon(Symbols.history_rounded),
+              size: 128.0,
+              color: context.colorScheme.primary,
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              "sync.export.history.empty.description".t(context),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
