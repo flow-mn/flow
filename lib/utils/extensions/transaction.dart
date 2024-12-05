f74f4f050ce77936c8a14e44cbfe7ec09de4c406
Batmend Ganbaatar <batmend@gege.mn>
2024-12-06 00:11:18 +0800

    Enahcnements

--- lib/utils/extensions/transaction.dart
+++ lib/utils/extensions/transaction.dart
@@ -2,15 +2,19 @@ import "package:flow/entity/transaction.dart";
 import "package:moment_dart/moment_dart.dart";
 
 extension TransactionHelpers on Transaction {
-  bool confirmable([DateTime? anchor]) =>
-      isPending == true &&
-      transactionDate.isPastAnchored(
-        anchor ?? Moment.now().endOfNextMinute(),
-      );
+  bool confirmable([DateTime? anchor]) {
+    if (isPending != true) return false;
 
-  bool holdable([DateTime? anchor]) =>
-      isPending != true &&
-      transactionDate.isFutureAnchored(
-        anchor ?? Moment.now().startOfMinute(),
-      );
+    return transactionDate.isPastAnchored(
+      anchor ?? Moment.now().endOfNextMinute(),
+    );
+  }
+
+  bool holdable([DateTime? anchor]) {
+    if (isPending != true) return false;
+
+    return transactionDate.isFutureAnchored(
+      anchor ?? Moment.now().startOfMinute(),
+    );
+  }
 }
