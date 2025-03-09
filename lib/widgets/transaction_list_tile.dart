442e03dabf1b51b9d6050d6bfc24e8a878c529d3
Batmend Ganbaatar <batmend@gege.mn>
2025-03-10 00:44:27 +0800

    don't show conversion when showing transfers separately

--- lib/widgets/transaction_list_tile.dart
+++ lib/widgets/transaction_list_tile.dart
@@ -162,7 +162,8 @@ class TransactionListTile extends StatelessWidget {
                       ),
                       overrideObscure: overrideObscure,
                     ),
-                    if (AccountsProvider.of(context).ready &&
+                    if (combineTransfers &&
+                        AccountsProvider.of(context).ready &&
                         transaction.extensions.transfer?.conversionRate !=
                             null &&
                         transaction.extensions.transfer?.conversionRate != 1.0)
