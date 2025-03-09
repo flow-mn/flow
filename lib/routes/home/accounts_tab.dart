d5ec996f68fdcac8fe4f9213c1873db28851cc72
Batmend Ganbaatar <batmend@gege.mn>
2025-03-09 23:40:55 +0800

    Fix account card balance wasn't getting updated

--- lib/routes/home/accounts_tab.dart
+++ lib/routes/home/accounts_tab.dart
@@ -14,7 +14,6 @@ import "package:flow/widgets/general/frame.dart";
 import "package:flow/widgets/general/spinner.dart";
 import "package:flow/widgets/home/home/account/no_accounts.dart";
 import "package:flow/widgets/home/home/account/total_balance.dart";
-import "package:flow/widgets/transaction_watcher.dart";
 import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
 import "package:material_symbols_icons/symbols.dart";
@@ -34,94 +33,86 @@ class _AccountsTabState extends State<AccountsTab>
   Widget build(BuildContext context) {
     super.build(context);
 
-    return TransactionWatcher(
-      builder: (context, _, __) {
-        final accounts = AccountsProvider.of(context).activeAccounts;
-        final bool ready = AccountsProvider.of(context).ready;
+    final accounts = AccountsProvider.of(context).activeAccounts;
+    final bool ready = AccountsProvider.of(context).ready;
 
-        if (!ready) {
-          return const Spinner();
-        }
+    if (!ready) {
+      return const Spinner();
+    }
 
-        return switch (accounts.length) {
-          0 => const NoAccounts(),
-          _ => Column(
-            children: [
-              const SizedBox(height: 16.0),
-              Frame(child: buildHeader(context)),
-              ValueListenableBuilder(
-                valueListenable: UserPreferencesService().valueNotiifer,
-                builder: (context, userPreferences, child) {
-                  final bool excludeTransfersInTotal =
-                      userPreferences.excludeTransfersFromFlow;
+    return switch (accounts.length) {
+      0 => const NoAccounts(),
+      _ => Column(
+        children: [
+          const SizedBox(height: 16.0),
+          Frame(child: buildHeader(context)),
+          ValueListenableBuilder(
+            valueListenable: UserPreferencesService().valueNotiifer,
+            builder: (context, userPreferences, child) {
+              final bool excludeTransfersInTotal =
+                  userPreferences.excludeTransfersFromFlow;
 
-                  return Expanded(
-                    child:
-                        _reordering
-                            ? Frame(
-                              child: ReorderableListView.builder(
-                                padding: const EdgeInsets.only(bottom: 96.0),
-                                itemBuilder:
-                                    (context, index) => Padding(
-                                      key: ValueKey(accounts[index].uuid),
-                                      padding: const EdgeInsets.only(
-                                        bottom: 16.0,
-                                      ),
-                                      child: AccountCard(
-                                        account: accounts[index],
-                                        useCupertinoContextMenu: false,
-                                        excludeTransfersInTotal:
-                                            excludeTransfersInTotal == true,
-                                      ),
-                                    ),
-                                proxyDecorator: proxyDecorator,
-                                itemCount: accounts.length,
-                                onReorder:
-                                    (oldIndex, newIndex) =>
-                                        onReorder(accounts, oldIndex, newIndex),
-                              ),
-                            )
-                            : ListView(
-                              padding: const EdgeInsets.all(16.0),
-                              children: [
-                                TotalBalance(),
-                                const SizedBox(height: 16.0),
-                                Divider(),
-                                const SizedBox(height: 16.0),
-                                ...accounts.map(
-                                  (account) => Padding(
-                                    padding: const EdgeInsets.only(
-                                      bottom: 16.0,
-                                    ),
-                                    child: AccountCard(
-                                      account: account,
-                                      useCupertinoContextMenu: Platform.isIOS,
-                                      excludeTransfersInTotal:
-                                          excludeTransfersInTotal == true,
-                                      onTapOverride: Optional(() async {
-                                        await context.push(
-                                          "/account/${account.id}",
-                                        );
-                                        setState(() {});
-                                      }),
-                                    ),
+              return Expanded(
+                child:
+                    _reordering
+                        ? Frame(
+                          child: ReorderableListView.builder(
+                            padding: const EdgeInsets.only(bottom: 96.0),
+                            itemBuilder:
+                                (context, index) => Padding(
+                                  key: ValueKey(accounts[index].uuid),
+                                  padding: const EdgeInsets.only(bottom: 16.0),
+                                  child: AccountCard(
+                                    account: accounts[index],
+                                    useCupertinoContextMenu: false,
+                                    excludeTransfersInTotal:
+                                        excludeTransfersInTotal == true,
                                   ),
                                 ),
-                                AccountCardSkeleton(
-                                  onTap: () => context.push("/account/new"),
+                            proxyDecorator: proxyDecorator,
+                            itemCount: accounts.length,
+                            onReorder:
+                                (oldIndex, newIndex) =>
+                                    onReorder(accounts, oldIndex, newIndex),
+                          ),
+                        )
+                        : ListView(
+                          padding: const EdgeInsets.all(16.0),
+                          children: [
+                            TotalBalance(),
+                            const SizedBox(height: 16.0),
+                            Divider(),
+                            const SizedBox(height: 16.0),
+                            ...accounts.map(
+                              (account) => Padding(
+                                padding: const EdgeInsets.only(bottom: 16.0),
+                                child: AccountCard(
+                                  account: account,
+                                  useCupertinoContextMenu: Platform.isIOS,
+                                  excludeTransfersInTotal:
+                                      excludeTransfersInTotal == true,
+                                  onTapOverride: Optional(() async {
+                                    await context.push(
+                                      "/account/${account.id}",
+                                    );
+                                    setState(() {});
+                                  }),
                                 ),
-                                const SizedBox(height: 16.0),
-                                const SizedBox(height: 64.0),
-                              ],
+                              ),
                             ),
-                  );
-                },
-              ),
-            ],
+                            AccountCardSkeleton(
+                              onTap: () => context.push("/account/new"),
+                            ),
+                            const SizedBox(height: 16.0),
+                            const SizedBox(height: 64.0),
+                          ],
+                        ),
+              );
+            },
           ),
-        };
-      },
-    );
+        ],
+      ),
+    };
   }
 
   Widget buildHeader(BuildContext context) {
