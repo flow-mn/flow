ceb9ce8addf532e3f59e4ee58d278567235c2755
Batmend Ganbaatar <batmend@gege.mn>
2025-03-28 17:28:35 +0800

    recurring extension draft

--- lib/entity/transaction/extensions/default/recurring.dart
+++ lib/entity/transaction/extensions/default/recurring.dart
@@ -1,26 +1,55 @@
 import "package:flow/entity/transaction/extensions/base.dart";
 import "package:flow/utils/utils.dart";
 import "package:json_annotation/json_annotation.dart";
+import "package:moment_dart/moment_dart.dart";
+import "package:recurrence/recurrence.dart";
 
 part "recurring.g.dart";
 
-/// By weekdays - mo, tu, we, th, fr, sa, su
-/// By monthdays - 1 - 31 (clamped to 28-31)
-/// Every n days
+@JsonSerializable(explicitToJson: true)
 class Recurring extends TransactionExtension implements Jasonable {
-  static const String name = "@flow/recurring";
+  static const String name = "@flow/default-recurring";
 
   @override
-  final String? relatedTransactionUuid;
+  String? relatedTransactionUuid;
+
+  @override
+  void setRelatedTransactionUuid(String uuid) => relatedTransactionUuid = uuid;
 
   /// [moment_dart](https://pub.dev/packages/moment_dart) compatible TimeRange string
   ///
   /// If null, same as [Moment.minValue] to [Moment.maxValue]
   ///
   /// This is inclusive on both ends
-  final String? range;
+  String? range;
 
-  final List<String> rules;
+  @JsonKey(includeToJson: false, includeFromJson: false)
+  TimeRange? get timeRange => range != null ? TimeRange.parse(range!) : null;
+
+  set timeRange(TimeRange? value) => range = value?.toString();
+
+  /// [recurrence](https://pub.dev/packages/recurrence) compatible rules
+  List<String> rules;
+
+  @JsonKey(includeToJson: false, includeFromJson: false)
+  List<RecurrenceRule> get recurrenceRules =>
+      rules.map((rule) => RecurrenceRule.parse(rule)).toList();
+
+  set recurrenceRules(List<RecurrenceRule> value) =>
+      rules = value.map((rule) => rule.serialize()).toList();
+
+  @JsonKey(includeToJson: false, includeFromJson: false)
+  Recurrence get recurrence => Recurrence(
+    range: timeRange ?? TimeRange.allTime(),
+    rules: recurrenceRules,
+  );
+
+  Recurring({
+    required this.relatedTransactionUuid,
+    required this.range,
+    required this.rules,
+    required super.uuid,
+  }) : super();
 
   @override
   @JsonKey(includeToJson: true)
